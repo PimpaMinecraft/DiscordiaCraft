8f3ea71aaa73e7a73f483809967dfa35716206d7
Juma7C9 <juma993+git@gmail.com>
2013-09-18 02:16:07 +0200

    Modify backup script

--- minecraft_backup.sh
+++ minecraft_backup.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
 # Minecraft directory goes here
-mcdir=~/useless_dir
+mcdir=
 
 # Backup directory goes here
-bkdir=~/backup_dir
+bkdir=
+
+# User script will be run with
+user=
 
 if [[ ! -e $mcdir ]]
 then
-	echo "[ERROR]: minecraft directory not found or mcdir variable not defined."
-	exit 0
+        echo "[ERROR]: minecraft directory not found or mcdir variable not set."
+        exit 0
 fi
 
 if [[ ! -e $bkdir ]]
 then
-        echo "[ERROR]: backup directory not found or bkdir variable not defined."
-        exit 0
+    echo "[ERROR]: backup directory not found or bkdir variable not set."
+    exit 0
 fi
 
 time=`date +%F_%H-%M`
 
-cd $bkdir
-tar -czf "minecraft-${time}.tar.gz" ${mcdir}/*
+cd $mcdir
+sudo -H -u $user tar -czf "${bkdir}/minecraft_discordia-${time}.tar.gz" ./*
 
 return_code=$?
 
 if [[ ! return_code -eq 0 ]]
 then
-	echo "[ERROR]: tar failed, and exited with code $return_code."
-	exit 0
+        echo "[ERROR]: tar failed, and exited with code $return_code."
+        exit 0
 fi
 
-echo "[OK] Backup suceeded. Data saved in $bkdir/minecraft-${time}.tar.gz"
+echo "[OK] Backup suceeded. Data saved in $bkdir/minecraft_discordia-${time}.tar.gz"
 exit 1
