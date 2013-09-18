fe82423bae376e862d95614a899354d974126e32
Juma7C9 <juma993+git@gmail.com>
2013-09-18 02:28:19 +0200

    Add basename in backup script

--- minecraft_backup.sh
+++ minecraft_backup.sh
@@ -9,30 +9,33 @@ bkdir=
 # User script will be run with
 user=
 
+# Backup name goes here
+bkname=minecraft
+
 if [[ ! -e $mcdir ]]
 then
-        echo "[ERROR]: minecraft directory not found or mcdir variable not set."
+	echo "[ERROR]: minecraft directory not found or mcdir variable not set."
         exit 0
 fi
 
 if [[ ! -e $bkdir ]]
 then
-    echo "[ERROR]: backup directory not found or bkdir variable not set."
-    exit 0
+	echo "[ERROR]: backup directory not found or bkdir variable not set."
+	exit 0
 fi
 
 time=`date +%F_%H-%M`
 
 cd $mcdir
-sudo -H -u $user tar -czf "${bkdir}/minecraft_discordia-${time}.tar.gz" ./*
+sudo -H -u $user tar -czf "${bkdir}/${bkname}_${time}.tar.gz" ./*
 
 return_code=$?
 
 if [[ ! return_code -eq 0 ]]
 then
-        echo "[ERROR]: tar failed, and exited with code $return_code."
-        exit 0
+	echo "[ERROR]: tar failed, and exited with code $return_code."
+	exit 0
 fi
 
-echo "[OK] Backup suceeded. Data saved in $bkdir/minecraft_discordia-${time}.tar.gz"
+echo "[OK] Backup suceeded. Data saved in $bkdir/${bkname}_${time}.tar.gz"
 exit 1
